914deb9e257ef7797b93190f841405388c07e812
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-11-08 22:14:42 +0500

    done

---
+++ script.js
@@ -0,0 +1,64 @@
+// let kuchbhi = "kashan"
+//
+/// String, Number or Boolean
+
+// let num = 1;
+
+// num += 1;
+
+// alert(num)
+
+// let array= []
+
+// for of, for in, for, for each
+
+// repeated loop
+
+// declaration,  condition, incrementation
+// variable banana
+// k s
+//  condition is true
+
+// true
+// true
+// true
+// true
+// true
+// true
+// true
+// true
+// true
+// false
+
+// for (let i = 1; i < 9; i += 1) {
+//     console.log("Hello World")
+// }
+
+// let array = ["Kashan", "wasay", "anus", "rehman"];
+
+// for (let index in array) {
+//   let name = array[index];
+//   console.log(name.toUpperCase());
+// }
+
+// let array = ["Kashan", "wasay", "anus", "rehman"];
+
+// for (let value of array){
+//     console.log(value);
+
+// }
+
+// let array = ["Kashan", "wasay", "anus", "rehman"];
+
+// const names = array.forEach((value) => {
+//   return value.toUpperCase();
+// });
+// console.log(names);
+
+
+
+
+// const kashan = array.map((value) => {
+// return value.toUpperCase();
+// });
+// console.log(kashan);
