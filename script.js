e7dd9dc6de93eb3d79c83025c7f0f3f59bddb59e
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-11-09 21:45:21 +0500

    done

--- script.js
+++ script.js
@@ -55,19 +55,94 @@
 // });
 // console.log(names);
 
-const numbers = [3, 7, 10, 21, 5];
-Tasks:
+// const numbers = [3, 7, 10, 21, 5];
+// Tasks:
 
-Print every number (use for)
+// Print every number (use for)
 
-Print only even numbers (use for-of)
+// Print only even numbers (use for-of)
 
-Find sum of all numbers
-
-
-
+// Find sum of all numbers
 
 // const kashan = array.map((value) => {
 // return value.toUpperCase();
 // });
 // console.log(kashan);
+
+// 5 tpyes of loops in javascript
+// for of
+// for in
+// for
+// forEach
+// map
+
+// repatation
+
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+// alert("hello")
+
+// for loop
+
+// declaration, condition, incrementation
+// let i = 0    i > 10     i += 1
+
+// true
+// true
+// concatination
+// let table = 5
+// for (let i = 1; i < 16; i += 1) {
+//   console.log(table + " X " + i + " = " + table * i);
+// }
+
+// for of
+// for in
+// for each
+//                 0               1              2
+let students = ["Anus Adnan", "Wasay Rashid", "Laptop Mouse", "Kashan Adnan"];
+
+// for (let student of students){
+//     console.log(student.split(' ')[0]);
+// }
+
+// for (let student in students){
+//     console.log(student);
+// }
+
+// forEach
+// map
+// method
+
+// console.log(students.forEach(element => {
+//     return element
+// }))
+
+// console.log(students.map((element) =>{
+//     return element.split(" ")[1]
+// }))
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+// while
+
+// let i = 0;
+// while (i > 10) {
+//   console.log(i);
+//   i += 1;
+// }
+
+let i = 0;
+do {
+  console.log(i);
+  i += 1;
+} while (i > 10);
